98e9f52895416664b9f2598d7be831d4110561af
Antoine Grondin <antoinegrondin@gmail.com>
2015-12-06 16:34:30 -0500

    stub server that counts requests per second on some pages

---
+++ watch.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if ! hash go; then
+    echo "you need Go"
+    exit
+fi
+
+if ! hash rerun; then
+    go get -u -f -v github.com/skelterjohn/rerun
+fi
+
+if ! hash humanlog; then
+    go get -u -f -v github.com/aybabtme/humanlog
+fi
+
+rerun github.com/aybabtme/fail.run/svc/cmd/failrund -port 3000 2>&1 | humanlog
