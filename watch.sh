d28731802810d277e5bd4077c252e37ec2846d4e
Antoine Grondin <antoinegrondin@gmail.com>
2015-12-06 16:47:21 -0500

    fix watch script

--- watch.sh
+++ watch.sh
@@ -10,7 +10,7 @@ if ! hash rerun; then
 fi
 
 if ! hash humanlog; then
-    go get -u -f -v github.com/aybabtme/humanlog
+    go get -u -f -v github.com/aybabtme/humanlog/...
 fi
 
 rerun github.com/aybabtme/fail.run/svc/cmd/failrund -port 3000 2>&1 | humanlog
